51bc4053c8aa7a0390cc6ef0e967988a40a1c106
Jackie-Milkydaytripper <31907958+Jackie-Milkydaytripper@users.noreply.github.com>
2017-09-17 14:28:49 -0400

    Add files via upload
    Mining Project: part 1
    Jackie

---
+++ MiningTime.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiningTime : MonoBehaviour {
+
+	bool  printedMessage = false;
+
+	// Use this for initialization
+	void Start () {
+		print ("Start");
+		printedMessage = false;
+
+	}
+		
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time > 3 && printedMessage == false) {
+			print ("It has been three seconds!"); 
+			printedMessage = true;
+
+		}
+
+	}
+}
